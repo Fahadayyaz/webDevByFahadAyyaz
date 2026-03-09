25681acd19fd825fc0e0af1b0c81f872941b20e1
Fahad Ayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2026-03-09 14:31:11 +0500

    Day02 Linking files and some tags

---
+++ Day02/script.js
@@ -0,0 +1,6 @@
+/* this is how we comment in JavaScript
+for multi lines
+*/
+// this is a single line commment
+// here is an alert function that apears on the top of a webpage
+alert("Day2 Linking Successful!");
